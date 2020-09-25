619ad1d0f59a9a3a78f7c5bd78e89bcacd4e9bd0
LucasVz <49080941+LucasVz@users.noreply.github.com>
2020-09-24 21:09:18 -0300

    oi

---
+++ lista1.js
@@ -0,0 +1,52 @@
+function mudar01(){
+document.getElementById("result").innerHTML = "mudou!"
+}
+
+
+function mudar(){ 
+    document.getElementById("luz1").src="Lon.png"
+}
+
+function mudar2(){ 
+    document.getElementById("luz1").src="Loff.png"
+}
+
+function mudar3(){ 
+    document.getElementById("luz2").src="Lon.png"
+}
+
+function mudar4(){ 
+    document.getElementById("luz2").src="Loff.png"
+}
+
+function mudar5(){ 
+    document.getElementById("luz3").src="Lon.png"
+}
+
+function mudar6(){ 
+    document.getElementById("luz3").src="Loff.png"
+}
+
+function mudar7(){ 
+    document.getElementById("luz4").src="Lon.png"
+}
+
+function mudar8(){ 
+    document.getElementById("luz4").src="Loff.png"
+}
+
+function mudar9(){ 
+    document.getElementById("luz5").src="Lon.png"
+}
+
+function mudar10(){ 
+    document.getElementById("luz5").src="Loff.png"
+}
+
+function mudar11(){ 
+    document.getElementById("luz6").src="Lon.png"
+}
+
+function mudar12(){ 
+    document.getElementById("luz6").src="Loff.png"
+}
